9c64dfe6903164e820065c15c60ca4872b20d818
Lyuiss <luiscmduarte077@gmail.com>
2025-02-21 22:49:46 -0300

    Ajustes no 1º exercício

--- 2025-02-21/Program.cs
+++ 2025-02-21/Program.cs
@@ -1,9 +1,12 @@
 using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Olá, mundo! Este é o meu primeiro exercício em C#.");
+        Console.WriteLine("Agora estou fazendo ajustes no código para praticar mais!");
     }
 }

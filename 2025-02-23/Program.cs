9eed6d9c54981e4131c9cea6786672b2be542a35
Lyuiss <luiscmduarte077@gmail.com>
2025-02-23 14:24:52 -0300

    Adicionado array e loop para exibir números

--- 2025-02-23/Program.cs
+++ 2025-02-23/Program.cs
@@ -9,3 +9,17 @@ class Program
 }
 
 Console.WriteLine("Este é o primeiro commit de alteração do terceiro dia.");
+
+static int Soma(int a, int b)
+{
+    return a + b;
+}
+
+int resultado = Soma(5, 3);
+Console.WriteLine($"O resultado da soma é: {resultado}");
+
+int[] numeros = { 1, 2, 3, 4, 5 };
+foreach (int numero in numeros)
+{
+    Console.WriteLine($"Número: {numero}");
+}

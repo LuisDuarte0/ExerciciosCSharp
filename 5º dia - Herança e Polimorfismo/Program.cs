09d34be15e8b757293d9cd91d2e89075b60659a7
Lyuiss <luiscmduarte077@gmail.com>
2025-02-24 15:06:49 -0300

    Adicionada classe base Animal

--- 5º dia - Herança e Polimorfismo/Program.cs
+++ 5º dia - Herança e Polimorfismo/Program.cs
@@ -6,5 +6,23 @@ class Program
     {
         Console.WriteLine("Bem-vindo ao quinto dia de treinamento em C#!");
         Console.WriteLine("Hoje vamos trabalhar com Herança e Polimorfismo.");
+
+        
     }
 }
+
+class Animal
+{
+    public string Nome { get; set; }
+
+    public virtual void EmitirSom()
+    {
+        Console.WriteLine("Som genérico de animal.");
+    }
+}
+
+Animal animal = new Animal
+{
+    Nome = "Animal Genérico"
+};
+animal.EmitirSom();

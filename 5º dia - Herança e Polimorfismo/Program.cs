b79473b89e9fa47c1efce71da5ddba05e03c20e8
Lyuiss <luiscmduarte077@gmail.com>
2025-02-24 15:05:53 -0300

    Adicionado arquivo inicial do quinto dia

---
+++ 5º dia - Herança e Polimorfismo/Program.cs
@@ -0,0 +1,10 @@
+ using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao quinto dia de treinamento em C#!");
+        Console.WriteLine("Hoje vamos trabalhar com Herança e Polimorfismo.");
+    }
+}

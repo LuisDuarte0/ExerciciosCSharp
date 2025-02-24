63a0d2274adca6e71179e6eedb6cbc72980de86b
Lyuiss <luiscmduarte077@gmail.com>
2025-02-24 15:00:02 -0300

    Adicionado arquivo inicial do quarto dia

---
+++ 4º dia - Classes e Objetos/Program.cs
@@ -0,0 +1,10 @@
+ using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao quarto dia de treinamento em C#!");
+        Console.WriteLine("Hoje vamos trabalhar com Classes e Objetos.");
+    }
+}

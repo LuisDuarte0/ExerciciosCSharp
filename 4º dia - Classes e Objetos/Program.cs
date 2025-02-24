14ef959c0e6f416a85952c3411ac964eae65eeb2
Lyuiss <luiscmduarte077@gmail.com>
2025-02-24 15:00:42 -0300

    Adicionada classe Pessoa e método Apresentar

--- 4º dia - Classes e Objetos/Program.cs
+++ 4º dia - Classes e Objetos/Program.cs
@@ -5,6 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Bem-vindo ao quarto dia de treinamento em C#!");
-        Console.WriteLine("Hoje vamos trabalhar com Classes e Objetos.");
+        Console.WriteLine("Hoje irei trabalhar com Classes e Objetos.");
     }
 }
+
+
+class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
+    }
+}
+
+Pessoa pessoa1 = new Pessoa
+{
+    Nome = "Luis",
+    Idade = 20
+};
+pessoa1.Apresentar();

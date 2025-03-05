0f8efa635216d821e390e352e0722f46a2db6008
Lyuiss <luiscmduarte077@gmail.com>
2025-03-05 18:23:11 -0300

    Adicionado funcionalidades de listas e LINQ no 7º dia

--- 7º dia - Coleções e LINQ/Program.cs
+++ 7º dia - Coleções e LINQ/Program.cs
@@ -1,4 +1,4 @@
- using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,5 +8,21 @@ class Program
     {
         Console.WriteLine("Bem-vindo ao sétimo dia de treinamento em C#!");
         Console.WriteLine("Hoje vamos trabalhar com Coleções e LINQ.");
+
+        // Criando uma lista de números
+        List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Usando LINQ para filtrar números pares
+        var numerosPares = numeros.Where(n => n % 2 == 0).ToList();
+
+        Console.WriteLine("Números pares:");
+        foreach (var numero in numerosPares)
+        {
+            Console.WriteLine(numero);
+        }
+
+        // Usando LINQ para calcular a soma dos números
+        int soma = numeros.Sum();
+        Console.WriteLine($"Soma dos números: {soma}");
     }
 }

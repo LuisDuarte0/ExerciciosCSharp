0b0de4b77355611e0964ee3f722bdae5ccc69f17
Lyuiss <luiscmduarte077@gmail.com>
2025-03-05 18:19:51 -0300

    Adicionado arquivo inicial do 9º dia

---
+++ 9º dia - Programação Assíncrona/Program.cs
@@ -0,0 +1,11 @@
+ using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao nono dia de treinamento em C#!");
+        Console.WriteLine("Hoje vamos trabalhar com Programação Assíncrona.");
+    }
+}

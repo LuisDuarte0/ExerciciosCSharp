f0add0229e3f63d7a7cb48923caa5cf0e9ab61a0
Lyuiss <luiscmduarte077@gmail.com>
2025-03-05 18:19:19 -0300

    Adicionado arquivo inicial do 8º dia

---
+++ 8º dia - Manipulação de Arquivos/Program.cs
@@ -0,0 +1,11 @@
+ using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao oitavo dia de treinamento em C#!");
+        Console.WriteLine("Hoje vamos trabalhar com Manipulação de Arquivos.");
+    }
+}

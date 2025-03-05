e13a5a8263663e55762e8b083bd5a41c4c47388f
Lyuiss <luiscmduarte077@gmail.com>
2025-03-05 18:18:55 -0300

    Adicionado arquivo inicial do 7º dia

---
+++ 7º dia - Coleções e LINQ/Program.cs
@@ -0,0 +1,12 @@
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo ao sétimo dia de treinamento em C#!");
+        Console.WriteLine("Hoje vamos trabalhar com Coleções e LINQ.");
+    }
+}
